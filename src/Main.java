3fc41ebef2bcb628e664b78c82c76ca5654bc4de
Inushi <62785480+Inushi@users.noreply.github.com>
2023-08-06 15:04:29 +0530

    IP address 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Node node1 = new Node("192.168.0.101", 5000);
-        Node node2 = new Node("192.168.0.102", 5001);
+        Node node1 = new Node("172.20.10.6", 5000);
+        Node node2 = new Node("172.20.10.8", 5001);
+        Node node3 = new Node("172.20.10.14", 5002);
 
         node1.joinNetwork(node2.getIpAddress());
-        node2.joinNetwork(node1.getIpAddress());
+        node1.joinNetwork(node3.getIpAddress());
 
+        // Start broadcasting messages
         node1.broadcastMessage("Hello from node 1!");
     }
+
 }
